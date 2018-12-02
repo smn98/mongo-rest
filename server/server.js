ee5c118926529a3330ab2e3c169a3681c1d6e4a4
thenath1998 <rkz.suman@gmail.com>
2018-12-02 13:40:02 +0530

    get /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const {ObjectID} = require('mongodb');
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
@@ -28,6 +28,22 @@ app.get('/todos', (req, res) => {
         res.status(404).send(e);
     });
 });
+
+app.get('/todos/:id',(req,res) => {
+    let id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    User.findById(id).then((user) => {
+        if (!user) {
+            res.status(404).send();
+        }
+        res.send(user);
+    }).catch((e) => res.status(404).send());
+    
+})
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
