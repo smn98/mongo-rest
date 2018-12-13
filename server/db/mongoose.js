e379347434d96c7884738512508d0bb80b827a95
thenath1998 <rkz.suman@gmail.com>
2018-12-13 21:57:05 +0530

    config and test db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 let mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }).then(() => {
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }).then(() => {
     console.log('connected to database');
 }, () => {
     console.log("could not connect to db");
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('./config/config.js');
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -7,7 +8,7 @@ const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
