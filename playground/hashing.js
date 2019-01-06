a6155222c24dfcce670c9c1bed8f8ffa740b0cb6
thenath1998 <rkz.suman@gmail.com>
2019-01-06 19:16:48 +0530

    hashing password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,13 +1,28 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data ={
-    id:10
-}
+let password = '123abc!';
 
-let token = jwt.sign(data,'hello123');
-let decoded = jwt.verify(token,'hello123');
-console.log(decoded);
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
+
+let hashedPassword = '$2a$10$UI8G.Qcwe/qYjdJB9fVUBe2Ipguf3setT1365F19ZIj6HIzEFL0Zq';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+})
+
+// let data ={
+//     id:10
+// }
+
+// let token = jwt.sign(data,'hello123');
+// let decoded = jwt.verify(token,'hello123');
+// console.log(decoded);
 // let data = {
 //     id: 4
 // };
