92202943689666e73cbb5426841cd2e461dadd95
thenath1998 <rkz.suman@gmail.com>
2019-01-06 16:37:15 +0530

    get user and authenticate middleware

--- server/server.js
+++ server/server.js
@@ -2,10 +2,11 @@ require('./config/config.js');
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
-const {ObjectID} = require('mongodb');
+const { ObjectID } = require('mongodb');
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
+const { authenticate } = require('./middleware/authenticate');
 
 const app = express();
 const port = process.env.PORT;
@@ -35,57 +36,57 @@ app.get('/todos', (req, res) => {
 });
 
 //get a todo by id
-app.get('/todos/:id',(req,res) => {
+app.get('/todos/:id', (req, res) => {
     let id = req.params.id;
 
-    if(!ObjectID.isValid(id)){
+    if (!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
     Todo.findById(id).then((todo) => {
         if (!todo) {
             res.status(404).send();
         }
-        res.send({todo});
-    }).catch((e) => res.status(404).send());    
+        res.send({ todo });
+    }).catch((e) => res.status(404).send());
 });
 
 //delete a todo by id
-app.delete('/todos/:id',(req,res) => {
+app.delete('/todos/:id', (req, res) => {
     let id = req.params.id;
 
-    if(!ObjectID.isValid(id)){
+    if (!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
     Todo.findByIdAndRemove(id).then((todo) => {
-        if(!todo){
+        if (!todo) {
             res.status(404).send();
         }
-        res.send({todo});
-    }).catch((e)=>res.status(400).send());
+        res.send({ todo });
+    }).catch((e) => res.status(400).send());
 });
 
 //update todo by id
-app.patch('/todos/:id',(req,res) => {
+app.patch('/todos/:id', (req, res) => {
     let id = req.params.id;
     let body = _.pick(req.body, ['text', 'completed']);
 
-    if(!ObjectID.isValid(id)){
+    if (!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
 
-    if(_.isBoolean(body.completed) && body.completed){
+    if (_.isBoolean(body.completed) && body.completed) {
         body.completedAt = new Date().getTime();
-    }else{
+    } else {
         body.completed = false;
         body.completedAt = null;
     }
 
-    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo)=>{
-        if(!todo){
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+        if (!todo) {
             return res.status(404).send();
         }
-        res.send({todo});
-    }).catch((e)=>{
+        res.send({ todo });
+    }).catch((e) => {
         res.status(404).send();
     })
 });
@@ -98,11 +99,16 @@ app.post('/users', (req, res) => {
 
     user.save().then(() => {
         return user.generateAuthToken();
-    }).then((token)=>{
-        res.header('x-auth',token).send(user);
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
     }).catch((e) => {
         res.status(404).send(e);
     });
+});
+
+//get user
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 
