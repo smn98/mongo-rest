b91f0dd3c75d9433a297ddd087c61d27dd33f659
thenath1998 <rkz.suman@gmail.com>
2019-01-07 22:43:23 +0530

    logging out user, test is broken

--- server/server.js
+++ server/server.js
@@ -132,6 +132,16 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
+//logging out user
+app.delete('/users/me/token', authenticate, (req, res) => {
+    console.log('hello');
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send();
+    });
+});
+
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 });
